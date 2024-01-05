d659f1d20267e275c0c2663e2d674883d2480b2b
iGWE-eDWIN <igweedwin15@gmail.com>
2024-01-05 03:43:59 +0100

    Second Commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,8 +3,12 @@ require('dotenv').config();
 
 // Create a new database
 const newDataBase = async () => {
-  await mongoose.connect(process.env.mongoDB_Url, {
-    useNewUrlParser: true,
-  });
+  try {
+    await mongoose.connect(process.env.mongoDB_Url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.error('Error connecting to MongoDB:', error.message);
+  } catch (error) {}
 };
 newDataBase();
