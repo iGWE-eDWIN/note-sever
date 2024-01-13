095ddcaec182d496bd762aa34e5fd7af654d1861
iGWE-eDWIN <igweedwin15@gmail.com>
2024-01-13 02:18:17 +0100

    9th commit

--- src/index.js
+++ src/index.js
@@ -20,18 +20,11 @@ app.use((req, res, next) => {
     'https://note-server.netlify.app'
   );
   // Allow the following HTTP methods
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, PATCH'
-  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
   // Allow the following headers
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Content-Type',
-    'Authorization'
-  );
+  res.header('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
   // Allow credentials (if needed)
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Credentials', 'true');
 
   // Continue with the request
   next();
